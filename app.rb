077a04423853a0b6325b0b4459afaa057d3203ce
Adrian Madrid <aemadrid@aemlnx.(none)>
2011-02-11 15:32:45 -0700

    ...

--- app.rb
+++ app.rb
@@ -4,6 +4,8 @@ require "bundler/setup"
 require 'sinatra'
 require 'org.torquebox.torquebox-messaging-client'
 
+@cnt, @time, @avg = 0, 0.0, 0.0
+
 get '/' do
   "Hello Producr TorqueBox!"
 end
@@ -14,6 +16,9 @@ get '/upcase/:term' do
   time = Time.now
   result = queue.publish_and_receive params[:term], :timeout => 1000
   time = Time.now - time
-  puts "Producr :: Got upcased term [#{result}] #{result.inspect} (#{result.class.name}) in #{'%.2f' % time} secs"
+  @cnt += 1
+  @time += time
+  @avg = @cnt / @time
+  puts "Producr :: Got upcased term [#{result}] #{result.inspect} (#{result.class.name}) in #{'%.2f' % time} secs (#{'%.2f  rps / %i / %.2f' % [@avg, @cnt, @time]})"
   result
 end
